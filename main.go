2a248a804b6a573ee15e587ab0f38b58033f87b9
hieutrtr <hieutrantrung.it@gmail.com>
2020-09-04 22:28:25 +0700

    add router middlewares

--- main.go
+++ main.go
@@ -1,4 +1,11 @@
 package main
 
+import(
+	"prime-service/router"
+	echoSwagger "github.com/swaggo/echo-swagger" // echo-swagger middleware
+)
+
 func main() {
+	r := router.New()
+	r.GET("/swagger/*", echoSwagger.WrapHandler)
 }
